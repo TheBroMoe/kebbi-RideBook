14eb1d300d92574777943cce5c9b3b89b5195ef4
thebromoe <kebbi@ualberta.ca>
2019-09-20 22:34:12 -0600

    Almost done java classes

--- code/rideTest/src/main.java
+++ code/rideTest/src/main.java
@@ -1,10 +1,18 @@
-public class main {
-    public static void main(String[] args) {
-        Date testDate = new Date("");
-        testDate.verifyInput();
-        System.out.println(testDate.isValidInput());
-        testDate.modifyInput("2020-2-29");
-        System.out.println(testDate.isValidInput());
+import RideAttributes.*;
 
+public class main {
+
+    private static int testAttributes(){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int testA = testAttributes();
+
+        if (testA == 0){
+            System.out.println("Everything Works!");
+        }else{
+            System.out.println(String.format("Test: %d, Failed", testA));
+        }
     }
 }
